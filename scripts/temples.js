d9ff67849af119bed01d7dc91a676042ce694886
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-06-22 18:44:12 -0400

    Temple update

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,12 @@ let lastModified = document.querySelector("#lastModified");
 const today = new Date();
 currentYear.textContent = today.getFullYear();
 lastModified.textContent = `Last Modification: ${document.lastModified}`
+
+//Code for hamburger button
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
