8fe8082cf0c3bf250a2c9d18b68ae4b360bde7c4
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-06-30 21:48:12 -0400

    update

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,13 @@ let lastModified = document.querySelector("#lastModified");
 const today = new Date();
 currentYear.textContent = today.getFullYear();
 lastModified.textContent = `Last Modification: ${document.lastModified}`
+
+const temperature = 95;
+const wind = 10;
+
+function calculateWindChill(temp, windSpeed) {
+    return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16)); 
+}
+
+let windChill = document.querySelector("#windChill");
+windChill.textContent = calculateWindChill(temperature, wind).toFixed(2);
