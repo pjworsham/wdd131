dd96f31879a9390490f632bcd817a1d41240e11c
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-07-14 19:04:12 -0400

    update

---
+++ week05/scripts/form.js
@@ -0,0 +1,34 @@
+let currentYear = document.querySelector("#currentYear");
+let lastModified = document.querySelector("#lastModified");
+const today = new Date();
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`
+
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        avgRating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+        {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+  ];
