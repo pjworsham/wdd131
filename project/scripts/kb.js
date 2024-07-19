aebc822a06d7bff9867ddc2348ae68f3a8d75122
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-07-18 22:06:22 -0400

    update

--- project/scripts/kb.js
+++ project/scripts/kb.js
@@ -1,3 +1,11 @@
+let currentYear = document.querySelector("#currentYear");
+let lastModified = document.querySelector("#lastModified");
+const today = new Date();
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`
+
+
+
 //Code for hamburger button
 const hamburgerElement = document.querySelector('#myButton');
 const navElement = document.querySelector('.menuLinks');
